14070d79461261d5082e6128b71ef84f71ce449c
Maria <magy_raise@mail.ru>
2020-05-25 09:54:52 +0200

    Version1-end

---
+++ js/gamburger.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {  
+    /*
+    1-Получить кнопку по которой будет клик
+    2-Получить мобильное меню
+    3-Прослушать клик по кнопке и по клику добавлять/удалять класс active у мобильного меню
+    4-Добавлять/удалять класс active у самой кнопки, по которой кликаем
+    5-Получить overlay
+    6-При клике по кнопке добавить/удалить класс active
+
+
+    Спрятать оверлэй
+    7-Прослушать события ресайз у окна
+    8-У дивов убрать эктив
+    */
+
+   /*1*/const mobileMenuToggle = document.querySelector('.toggle-menu');
+   /*2*/const mobMenu = document.querySelector('.header-nav');
+   /*5*/const overlay = document.querySelector('#overlay');
+   const logo = document.querySelector('.logo')
+
+   /*3*/mobileMenuToggle.addEventListener('click', function(){
+       mobMenu.classList.toggle('active');
+       /*4*/this.classList.toggle('active');
+       /*6*/overlay.classList.toggle('active');
+       logo.classList.toggle('active');
+   })
+
+   /*7*/window.addEventListener('resize', function(){
+        /*8*/mobMenu.classList.remove('active');
+        /*8*/mobileMenuToggle.classList.remove('active');
+        /*8*/overlay.classList.remove('active');
+        logo.classList.toggle('active');
+   })
+})
--- js/main.js
+++ js/main.js
@@ -1,59 +1,89 @@
 $(document).ready(function () {
 
     // Фильтрация проектов
-    let containerEl = document.querySelector('#portfolio-projects');
-    let mixer = mixitup(containerEl,{
-        classNames: {
-            block: ""
-        }
-    })
+    // let containerEl = document.querySelector('#portfolio-projects');
+    // let mixer = mixitup(containerEl,{
+    //     classNames: {
+    //         block: ""
+    //     }
+    // })
 
-    /*
-    1-Получить кнопку по которой будет клик
-    2-Получить мобильное меню
-    3-Прослушать клик по кнопке и по клику добавлять/удалять класс active у мобильного меню
-    4-Добавлять/удалять класс active у самой кнопки, по которой кликаем
-    5-Получить overlay
-    6-При клике по кнопке добавить/удалить класс active
-
-
-    Спрятать оверлэй
-    7-Прослушать события ресайз у окна
-    8-У дивов убрать эктив
-    */
-
-   /*1*/const mobileMenuToggle = document.querySelector('.toggle-menu');
-   /*2*/const mobMenu = document.querySelector('.header-nav');
-   /*5*/const overlay = document.querySelector('#overlay');
-   const logo = document.querySelector('.logo')
-
-   /*3*/mobileMenuToggle.addEventListener('click', function(){
-       mobMenu.classList.toggle('active');
-       /*4*/this.classList.toggle('active');
-       /*6*/overlay.classList.toggle('active');
-       logo.classList.toggle('active');
-   })
-
-   /*7*/window.addEventListener('resize', function(){
-        /*8*/mobMenu.classList.remove('active');
-        /*8*/mobileMenuToggle.classList.remove('active');
-        /*8*/overlay.classList.remove('active');
-        logo.classList.toggle('active');
-   })
-
-   //fake-placeholder .fake-placeholder
+   //fake-placeholder
    const formInputs = document.querySelectorAll('.card-field');
    for(let item of formInputs){
        const inputPlacegolder = item.nextElementSibling;
        item.addEventListener ('click', function(){
-            inputPlacegolder.classList.add('active');
+            inputPlacegolder.classList.add('contacts-active');
        })
        //Если мой элемент (импут) имеет такое событие как blur пусть в этом случае выполняется функция, которая проверяет сначала 
        //если мой этот импут если у него value (то что пользователь ввел) равен пустоте "", в этом случае удалить этот класс эктив у inputplaceholdera мы add меняем на remove
        item.addEventListener('blur', function (){
            if (this.value == ''){
-               inputPlacegolder.classList.remove('active');
+               inputPlacegolder.classList.remove('contacts-active');
            }
        })
+   }
+
+   //ВАЛИДАЦИЯ ФОРМЫ
+   //Скрипт валидации формы, проверка на заполненность полей (на почту)
+   //1-я нахожу на своей странице форму, т.е. я нахожу в своем документе див с id contacts-card
+   //2-и к этой форме я применяю метод/функция validate, это тот самый метод который описан в библиотеке jquery-validate которую мы добавили в наш проект
+   //3-создаем правило, по которому будет проверяться форма, правило пишется для каждого импута и импут выбирается по имени
+   //4- есть импут с именем имэл, правило-объект (в js если фигурные скобки, то передается обект), какой объект -requared- это поле, которое отвечает за заполненность в поле, за наличие какого-то текста в поле, и для этого параметра мы ставим true - т.е. поле обязательно должно быть заполнено
+   //5-есть метод email: -этот метод проверяет на корректность заполнения электронной почты, т.е. присутствует ли символ @ "эт"
+   //6-метод messages: будет показывать сооющения для пользователя, если како-либо из правил не выполнено, если не выполнились правила для email:, мы можем показать такой текст пользователю "Введите Ваш email", ну а еслине прошла проверка на корректность мы покажем такое сообщение 'Отсутствует символ @'.
+
+    $('#contacts-card').validate({
+        rules: {
+            email: {
+               required: true,
+               email: true
+            },
+            theme: {
+                required: true
+            },
+            message: {
+                required: true
+            }
+        },
+        messages: {
+            email: {
+                required: 'Введите Ваш email',
+                email: 'Отсутствует символ @'
+            },
+            theme: {
+                required: 'Введите тему сообщения'
+            },
+            message: {
+                required: 'Введите текст сообщения'
+            }
+        },
+
+        //Если все эти шаги прошли успешно, нам надо добавить следующее поле, следующую функцию
+        submitHandler: function (form) {
+            ajaxFormSubmit();
+        }
+   });
+   
+   //Функция AJAX запроса на сервер, этот запрос хороше тем, что при отправке формы  на сервер страница не перезагрузиться, мы останемся на той же стр с кот отправили форму.
+   function ajaxFormSubmit() {
+       let string = $("#contacts-card").serialize();//в переменную стринг, методом serialize из моей формы contacts-card Сохрани все данные введенные в форму, в виде строки.
+       //Сохраняем данные введенные в форму, в строку.
+
+       //Формируем AJAX запрос
+       $.ajax ({
+           type: "POST", //Типа запроса POST (есть два метода post и get)
+           url: "./php/php/mail.php", //Куда отправляем запрос   php\php\mail.php
+           data: string, //Что уйдёт? Какие данные отправляем, в данном случае отправляем переменную string
+
+           //Если все прошло успешно, используем функцию
+           success: function (html) {
+                $("#contacts-card").slideUp(800); //Форма плавно скрывается за 0,8секунды
+                $("#answer").html(html); //и диву с id = answer должен прописаться html-текст
+           }
+       });
+
+       //Чтобы после submit ничего не выполнялось - делаем возврат false чтобы прервать цепочку срабатывания остальных функций
+       return false;
    }
 })
